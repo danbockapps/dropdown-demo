aa78250fb063ebb3bbcfe5b7b1ec724dfcc2f803
Dan Bock <daniel.bock@sscinc.com>
2020-08-10 15:20:45 -0400

    Add DropDownBox demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DropDownBox, TreeView } from "devextreme-react";
+import React, { FC, useState } from "react";
+import "./App.css";
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.light.css";
+
+let initialdataSource: { id: number; name: string; selected: boolean }[] = [];
+
+for (let i = 0; i < 50; i++) {
+  initialdataSource.push({ id: i, name: `Option ${i}`, selected: false });
+}
 
 function App() {
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+  const dataSource = initialdataSource.map((e) => ({
+    ...e,
+    selected: selectedItems.includes(e.id),
+  }));
+
+  const TreeViewRender: FC = () => (
+    <TreeView
+      dataSource={dataSource}
+      keyExpr="id"
+      displayExpr="name"
+      selectionMode="multiple"
+      showCheckBoxesMode="normal"
+      onItemSelectionChanged={(e) =>
+        setSelectedItems(e.component?.getSelectedNodeKeys() || [])
+      }
+    />
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DropDownBox
+      dataSource={dataSource}
+      value={selectedItems}
+      valueExpr="id"
+      displayExpr="name"
+      contentRender={TreeViewRender}
+    />
   );
 }
 
